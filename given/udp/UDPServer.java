76eb2cde7c178a4c364b81e207bb166e36ea0703
Calvin Chan <cc6815@matrix15.doc.ic.ac.uk>
2017-02-16 13:42:02 +0000

    udpworks

--- given/udp/UDPServer.java
+++ given/udp/UDPServer.java
@@ -23,23 +23,25 @@ public class UDPServer {
 		int	pacSize;//fine
 		byte[]	pacData;//fine
 		DatagramPacket 	pac;//fine
-                
+
 		// TO-DO: Receive the messages and process them by calling processMessage(...).
 		//        Use a timeout (e.g. 30 secs) to ensure the program doesn't block forever
-		
+
 		System.out.println("Ready to recieve messages...");
 		boolean close = false;
 		while(!close){
-			
+
 			pacSize = 5000;
 			pacData = new byte[5000];
-		// setup package	
+		// setup package
 			pac = new DatagramPacket(pacData, pacSize);
-			
+
 			try{
 				//        Use a timeout (e.g. 30 secs) to ensure the program doesn't block forever
                                 recvSoc.setSoTimeout(30000);
 				recvSoc.receive(pac);
+						String d = new String (pac.getData()).trim();
+						  processMessage(d);
 			}
 			catch (IOException e){
 				System.out.println("Error IO exception receiving packet.");
@@ -47,49 +49,49 @@ public class UDPServer {
 				System.out.println("Now closing server...");
 				System.exit(-1);
 			}
-			String d = pac.toString();
-                             processMessage(d);
+
+
 		}
-		
+
 	}
 
-	
+
               public void processMessage(String data) {
 
 		MessageInfo msg = null;
 
 		// TO-DO: Use the data to construct a new MessageInfo object
-		
-		
-		
+
+
+
 		try {
-			
-			msg = new MessageInfo(data);	
-			
-		} 
-		
+
+			msg = new MessageInfo(data);
+
+		}
+
 		catch (Exception e) {
 			System.out.println("Error: IO exception creating ObjectInputStream.");
 		}
 		// try to implement class not found
-		
+
 		// TO-DO: On receipt of first message, initialise the receive buffer
-		
+
 		if(receivedMessages==null){
 			totalMessages = msg.totalMessages;
 			receivedMessages = new int[totalMessages];
 		}
 
 		// TO-DO: Log receipt of the message
-		
+
 		receivedMessages[msg.messageNum] = 1;
 
 		// TO-DO: If this is the last expected message, then identify
 		//        any missing messages
-		
+
 		if (msg.messageNum + 1 == msg.totalMessages) {
 			boolean close = true;
-			
+
 			String s = "Lost packet numbers: ";
 			int count = 0;
 			for (int i = 0; i < totalMessages; i++) {
@@ -98,12 +100,12 @@ public class UDPServer {
 					s = s + " " + (i+1) + ", ";
 				}
 			}
-			
+
 			if (count == 0) s = s + "None";
-			
+
 			System.out.println("Of " + msg.totalMessages + ", " + (msg.totalMessages - count) + " received successfully");
 			System.out.println("Of " + msg.totalMessages + ", " + count + " failed");
-			
+
 		}
 
 	}
@@ -112,16 +114,16 @@ public class UDPServer {
 	public UDPServer(int rp) {
 		// TO-DO: Initialise UDP socket for receiving data
 		try {
-			
+
 			recvSoc = new DatagramSocket(rp);
 		}
 		catch (SocketException e) {
 			System.out.println("Error: Could not create socket on port " + rp);
 			System.exit(-1);
 		}
-		
+
 		// Make it so the server can run.
-		
+
 		//close = false;
 	}
 
@@ -136,12 +138,12 @@ public class UDPServer {
 		recvPort = Integer.parseInt(args[0]);
 
 		// TO-DO: Construct Server object and start it by calling run().
-		
+
 		UDPServer udpsrv = new UDPServer(recvPort);
 		try {
 			udpsrv.run();
 		} catch (Exception e) {}
-		
+
 	}
 
 }
